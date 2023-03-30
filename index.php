37f18c54a228b050097dd2eb3cc44c34482db57b
Alexander <formysite.ru@yandex.ru>
2023-03-30 17:52:27 +0300

    begin

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Урок 8.2. Работа с XML. Создадим функцию для записи данных из XML в MySQL</title>
+    <link rel="stylesheet" href="CSS/normalize.css">
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+    <header></header>
+    <h1>
+        Урок 8.2. Работа с XML. Создадим функцию для записи данных из XML в MySQL
+    </h1>
+    <div class="block_title_php">
+        <?php
+        $nickname = " Александр!";
+        $hello = "Привет ";
+        $offer = "На этом занятии создадим функцию для записи данных из файла XML в MySQL и подготовим структуру базы данных MySQL для вставки данных из XML файла";
+        echo "<h2 class='title_php'>" . $hello . $nickname . "</h2>";
+        echo "<h4 class='subtitle_php'>" . $offer . "</h4>";
+        ?>
+    </div>
+    <section class="begin">
+
+    </section>
+    <section class=""></section>
+    <section class=""></section>
+    <section class=""></section>
+
+    <footer></footer>
+</body>
+
+</html>
